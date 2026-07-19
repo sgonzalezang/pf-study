cd8797ab1e1e8dbe30e55323f8c9ad358680d223
Santiago Gonzalez <43614841+sgonzalezang@users.noreply.github.com>
2026-07-18 21:11:20 -0500

    cache bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pf-v17';
+const CACHE='pf-v18';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})());});
 self.addEventListener('fetch',e=>{
